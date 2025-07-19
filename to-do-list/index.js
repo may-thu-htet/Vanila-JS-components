ba8a4497c37e9b46190a5372fb74cc88148485f8
May Thu Htet <maythuhtet2892@gmail.com>
2025-07-19 12:55:38 +0100

    To-do-list

---
+++ to-do-list/index.js
@@ -0,0 +1,26 @@
+import "./styles.css";
+
+// element from DOM
+let submitButton = document.getElementById("submit-button");
+let inputTask = document.getElementById("input-task");
+let ul = document.getElementById("list");
+
+// adding event listener to the submit button
+submitButton.addEventListener("click", doSubmit);
+
+//to do function when you clicked the submit button
+function doSubmit() {
+  // create li element
+  let li = document.createElement("li");
+  // create button element
+  let deleteButton = document.createElement("button");
+
+  li.textContent = inputTask.value;
+  deleteButton.textContent = "Delete";
+  ul.appendChild(li);
+  li.appendChild(deleteButton);
+  deleteButton.addEventListener("click", () => {
+    ul.removeChild(li);
+  });
+  inputTask.value = "";
+}
