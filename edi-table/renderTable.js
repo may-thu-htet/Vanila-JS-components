73dc4c89dbde0006375a81dfd5bd5dddff6fc28a
May Thu Htet <maythuhtet2892@gmail.com>
2025-07-22 11:48:24 +0100

    edit delete button added

--- edi-table/renderTable.js
+++ edi-table/renderTable.js
@@ -7,6 +7,9 @@ export function renderTableHeader(thead, users) {
     th.textContent = key[0].toUpperCase() + key.slice(1);
     tr.appendChild(th);
   });
+  const editDelete = document.createElement("th");
+  editDelete.textContent = "Edit/Delete";
+  tr.appendChild(editDelete);
   thead.appendChild(tr);
 }
 
@@ -26,6 +29,23 @@ export function renderTableBody(tbody, users, currentPage, noOfRows) {
       td.textContent = value;
       tr.appendChild(td);
     });
+
+    const actionTd = document.createElement("td");
+    const editBtn = document.createElement("button");
+    editBtn.setAttribute("class", "edit-button");
+    editBtn.textContent = "Edit";
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+    deleteBtn.setAttribute = ("class", "delete-button");
+
+    const buttonWrapper = document.createElement("div");
+    buttonWrapper.className = "buttons";
+    buttonWrapper.appendChild(deleteBtn);
+    buttonWrapper.appendChild(editBtn);
+
+    actionTd.appendChild(buttonWrapper);
+    tr.appendChild(actionTd);
     tbody.appendChild(tr);
   });
 }
