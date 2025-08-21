c27e6162405e1ab1c78aaef361ffd4a908f1bd84
May Thu Htet <maythuhtet2892@gmail.com>
2025-08-21 17:58:01 +0700

    added radio button logic

--- checkboxes and radio buttons/index.js
+++ checkboxes and radio buttons/index.js
@@ -11,3 +11,14 @@ checkboxForm.addEventListener("submit", (e) => {
 
   checkBoxText.textContent = `I checked ${checkboxInputs.join(",")} boxes.`;
 });
+
+const radioForm = document.querySelector(".radio-form");
+const radioText = document.querySelector(".radio-text");
+
+radioForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const selectedRadio = Array.from(document.querySelectorAll(".radio"))
+    .filter((radio) => radio.checked)
+    .map((radio) => radio.value);
+  radioText.textContent = `I selected ${selectedRadio}.`;
+});
